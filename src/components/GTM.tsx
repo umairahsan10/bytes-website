3b5398c9bceb02a3fe8ca69e640d020cd0ce35b9
ammarqasmi <ammarsqasmi@gmail.com>
2025-07-08 03:29:36 +0500

    GTM All Done --- CTA, domain switch and account switch needed

--- src/components/GTM.tsx
+++ src/components/GTM.tsx
@@ -1,41 +1,28 @@
-// components/GTM.tsx
-import Script from "next/script";
+'use client';
 
-export const GTM_ID = "GTM-5MHPQ34Q"; // Replace with your actual GTM ID
+import Script from 'next/script';
 
-/**
- * Google Tag Manager Script
- * Injects the GTM script into the document <head> using Next.js <Script> component.
- */
-const GTMHead: React.FC = () => (
-  <Script
-    id="gtm-head"
-    strategy="afterInteractive"
-    dangerouslySetInnerHTML={{
-      __html: `
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${GTM_ID}');
-      `,
-    }}
-  />
-);
+export default function GTM({ id }: { id: string }) {
+  return (
+    <Script id="gtm-head" strategy="afterInteractive">{`
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','${id}');
+    `}</Script>
+  );
+}
 
-/**
- * Google Tag Manager noscript fallback.
- * Should be rendered at the top of <body> for users with JavaScript disabled.
- */
-export const GTMNoScript: React.FC = () => (
-  <noscript>
-    <iframe
-      src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-      height="0"
-      width="0"
-      style={{ display: "none", visibility: "hidden" }}
-    />
-  </noscript>
-);
-
-export default GTMHead;
+export function GTMNoScript({ id }: { id: string }) {
+  return (
+    <noscript>
+      <iframe
+        src={`https://www.googletagmanager.com/ns.html?id=${id}`}
+        height="0"
+        width="0"
+        style={{ display: 'none', visibility: 'hidden' }}
+      />
+    </noscript>
+  );
+}
