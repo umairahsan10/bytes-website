53777b45c3d6974b3e1b575a5b22065acc0c89d4
BytesPlatform <developer@bytesplatform.com>
2025-08-29 03:09:04 +0500

    /services and /technologies removed

--- src/app/technologies/page.tsx
+++ src/app/technologies/page.tsx
@@ -1,19 +1,5 @@
-"use client";
-
-import { Header } from "@/sections/Navbar";
-import { Footer } from "@/sections/Footer";
+import { redirect } from "next/navigation";
 
 export default function TechnologiesPage() {
-  return (
-    <>
-      <Header />
-      <main className="min-h-screen flex flex-col items-center justify-center px-4 text-center">
-        <h1 className="text-5xl font-bold">Technologies</h1>
-        <p className="mt-6 max-w-xl text-lg text-gray-300">
-          A showcase of the tools and frameworks we love to use.
-        </p>
-      </main>
-      <Footer />
-    </>
-  );
+  redirect('/');
 } 
