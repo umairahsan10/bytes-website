4bb6bd8d75c59ee3fd860f4ffaf329f841a96038
Maaz <qazimaaz404@gmail.com>
2026-01-24 02:17:32 +0500

    polyfills removed

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   eslint: {           
     ignoreDuringBuilds: true,
   },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
   images: {
     remotePatterns: [
       {
